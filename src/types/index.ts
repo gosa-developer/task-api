78a40e6b83f92e8048a3b085d6f845c35100e1c1
gosa <gobenagosa1@gmail.com>
2026-03-26 20:37:09 +0300

    index types added

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export class AppError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message: string) {
+    super(message, 409);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
